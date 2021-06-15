65815c91fd7976f66ea3012a21a8b3b5e5f55348
Andres <andres.porfirio@gmail.com>
2021-06-15 14:21:09 -0300

    configured react project

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import Home from "./pages/home";
+import About from "./pages/about";
+
+export default function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
---
+++ src/pages/home/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import Map from "../../components/map";
+
+export default function (props) {
+  return (
+    <div>
+      <Map />
+    </div>
+  );
+}
