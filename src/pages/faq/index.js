26758140623f246bbf0db2fa09bddc2043e68e68
Andres <andres.porfirio@gmail.com>
2021-07-13 11:37:22 -0300

    added drive link to FAQ page - external URLs

--- src/pages/faq/index.js
+++ src/pages/faq/index.js
@@ -2,7 +2,10 @@ import React from "react";
 import { useHistory } from "react-router-dom";
 
 import TopMenu from "../../components/topmenu";
+import DriveIframe from "../../components/drive-iframe";
 import Footer from "../../components/footer";
+
+import externalURLs from "../../services/external-urls";
 
 import "./styles.css";
 
@@ -10,10 +13,12 @@ export default function FAQ(props) {
   const history = useHistory();
 
   return (
-    <div className="schedule">
+    <div className="drive-container">
       <div className="components-overlay">
         <TopMenu icon="arrow" title="FAQ" onClick={() => history.push("/")} />
-        FAQ
+
+        <DriveIframe url={externalURLs["drive-faq"]} />
+
         <Footer />
       </div>
     </div>
--- src/services/external-urls.js
+++ src/services/external-urls.js
@@ -1,5 +1,5 @@
 const externalURLs = {
-  //rooms
+  //Salas
   "sala-1": "https://meet.google.com/xco-fbfi-pfy",
   "sala-2": "https://meet.google.com/xco-fbfi-pfy",
   "sala-3": "https://meet.google.com/xco-fbfi-pfy",
@@ -11,12 +11,16 @@ const externalURLs = {
   "sala-9": "https://meet.google.com/xco-fbfi-pfy",
   "sala-10": "https://meet.google.com/xco-fbfi-pfy",
 
-  //novas:
+  //EVIN
   "evin-base": "https://eventos.utfpr.edu.br/",
 
-  //drive (publicar na web, depois usar link do iframe)
+  //Links do Drive (publicar na web, depois usar link do iframe)
+
   "drive-guia-autores":
     "https://docs.google.com/document/d/e/2PACX-1vS-c5YBqj6udQVB8nkYnbe0b3ohAT-0cRVBJAuv9t6uV0KBbqPVJvPCW-xmk5IBI7dBzZ8jac32Tp_1/pub?embedded=true",
+
+  "drive-faq":
+    "https://docs.google.com/document/d/e/2PACX-1vSi5LcB022Yh1eNnVCPf25BMtpFAExiV_e2PXxmRn6nUvS6HMrle0RujZ3eB_2koAA-cB3mbBLPvFXm/pub?embedded=true",
 };
 
 export default externalURLs;
