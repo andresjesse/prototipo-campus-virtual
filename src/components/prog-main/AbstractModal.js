8e94482ca20692f5a7f089a51d952a3cce013d40
Andres <andres.porfirio@gmail.com>
2021-07-14 14:17:47 -0300

    styled abstract modal

--- src/components/prog-main/AbstractModal.js
+++ src/components/prog-main/AbstractModal.js
@@ -1,15 +1,28 @@
 import React from "react";
 
+import icClose from "../../assets/icons/ic-close.png";
+
 import { AbstractContext } from ".";
 
 const AbstractModal = function () {
   const ctx = React.useContext(AbstractContext);
 
+  const close = () => ctx.setAbstract(null);
+
   if (ctx.abstract == null) return <div></div>;
 
   return (
-    <div className="abstract-modal" onClick={() => ctx.setAbstract(null)}>
-      {ctx.abstract}
+    <div className="abstract-modal-container" onClick={() => close()}>
+      <div className="abstract-modal global-shadow" onClick={() => close()}>
+        {ctx.abstract}
+
+        <img
+          src={icClose}
+          className="modal ic-close"
+          alt="ícone para fechar modal"
+          onClick={() => close()}
+        />
+      </div>
     </div>
   );
 };
