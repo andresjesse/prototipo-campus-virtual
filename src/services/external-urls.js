2978381f8bc08f47187d85f80d44ab43b0ca9387
Andres <andres.porfirio@gmail.com>
2021-07-29 14:46:04 -0300

    updated FAQ public url. generated production build

--- src/services/external-urls.js
+++ src/services/external-urls.js
@@ -4,7 +4,7 @@ const externalURLs = {
   "contact-facebook": "https://www.facebook.com/UTFPRgp",
   "contact-instagram": "https://www.instagram.com/utfprgp",
 
-  //Salas
+  //Salas (TEMP! Substituir)
   "sala-1": "https://meet.google.com/xco-fbfi-pfy",
   "sala-2": "https://meet.google.com/xco-fbfi-pfy",
   "sala-3": "https://meet.google.com/xco-fbfi-pfy",
@@ -21,11 +21,10 @@ const externalURLs = {
 
   //Links do Drive (publicar na web, depois usar link do iframe)
 
-  "drive-guia-autores":
-    "https://docs.google.com/document/d/e/2PACX-1vS-c5YBqj6udQVB8nkYnbe0b3ohAT-0cRVBJAuv9t6uV0KBbqPVJvPCW-xmk5IBI7dBzZ8jac32Tp_1/pub?embedded=true",
+  "drive-guia-autores": "",
 
   "drive-faq":
-    "https://docs.google.com/document/d/e/2PACX-1vSi5LcB022Yh1eNnVCPf25BMtpFAExiV_e2PXxmRn6nUvS6HMrle0RujZ3eB_2koAA-cB3mbBLPvFXm/pub?embedded=true",
+    "https://docs.google.com/document/d/e/2PACX-1vTaqzAUT7oI8cjzCRo12brbHsev2B_nNWxBLZkqG-Kibdp1oREn03IujxH77T_VE-Iz2H_C1TpAHtln/pub?embedded=true",
 };
 
 export default externalURLs;
